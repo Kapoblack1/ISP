894cc060b2ab0a82ca001269ba021e01666859e2
Cafe <Alepy@hotmail.com>
2023-06-18 23:54:33 +0100

    Radio commit

--- App.js
+++ App.js
@@ -9,6 +9,8 @@ import ImageUploadScreen from './app/screems/ImageUploadScreen';
 import VideoUploadScreen from './app/screems/VideoUploadScreen';
 import VideoListScreen from './app/screems/VideoListScreen';
 import AudioUploadScreen from './app/screems/AudioUploadScreen';
+import AudioListScreen from './app/screems/AudioListScreen';
+import RadioListScreen from './app/screems/RadioListScreen';
 
 // Importe os componentes de tela que você deseja navegar
 
@@ -19,7 +21,7 @@ const Stack = createStackNavigator();
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName='AudioUploadScreen'>
+      <Stack.Navigator initialRouteName='RadioListScreen'>
         <Stack.Screen name="Login" component={Login} options={{headerShown:true}}/>
         <Stack.Screen name="UserPage" component={UserPage} options={{headerShown:true}}/>
         <Stack.Screen name="ImageUploadScreen" component={ImageUploadScreen} options={{headerShown:true}}/>
@@ -28,6 +30,8 @@ const App = () => {
         <Stack.Screen name="VideoUploadScreen" component={VideoUploadScreen} options={{headerShown:true}}/>
         <Stack.Screen name="VideoListScreen" component={VideoListScreen} options={{headerShown:true}}/>
         <Stack.Screen name="AudioUploadScreen" component={AudioUploadScreen} options={{headerShown:true}}/>
+        <Stack.Screen name="AudioListScreen" component={AudioListScreen} options={{headerShown:true}}/>
+        <Stack.Screen name="RadioListScreen" component={RadioListScreen} options={{headerShown:true}}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
